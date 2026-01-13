ae5536e18a514de8b1032aa3cec421c12acaadc1
IzaqueSA <izaquesousadeandrade@gmail.com>
2026-01-13 18:58:05 +0000

    Conexão com API configurada - primeiros testes

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // script.js — código inicial
 console.log('script.js carregado');
-onload = () => {
-    const titulo = document.getElementById('capa-titulo');
-    const subtitulo = document.getElementById('capa-subtitutlo');
-    titulo.style.color = 'rgb(224, 0, 172)';
-    subtitulo.style.color = 'rgb(245, 245, 245)';
-    setInterval(() => {
-        const colors = ['rgb(224, 0, 172)', 'rgb(107, 0, 224)', 'rgb(135, 224, 0)'];
-        const currentColor = titulo.style.color;
-        const nextColor = colors[(colors.indexOf(currentColor) + 1) % colors.length];
-        const backColor = colors[(colors.indexOf(currentColor) + 2) % colors.length];
-        titulo.style.color = nextColor;
-        subtitulo.style.color = backColor;
-    }, 1000);
-}
+
+// Animação de mudança de cor do texto na capa
+
+    gsap.to("#capa-titulo", {
+        keyframes:
+            [
+                { duration: 2, ease: "power1.inOut", color: "#6B00E0" },
+                { duration: 2, ease: "power1.inOut", color: "#87E000" }
+            ],
+        repeat: -1,
+        yoyo: true
+    });
+    
+    gsap.to("#capa-subtitulo", {
+        keyframes: [
+            {duration: 2, ease: "power1.inOut", color: "#E000AC"},
+            {duration: 2, ease: "power1.inOut", color: "#6B00E0"}
+        ],
+        repeat: -1,
+        yoyo: true
+    });
+
 
 function radioButton() {
     const radio = document.querySelector('#material-symbols-outlined-radio-button');
@@ -30,3 +38,17 @@ window.addEventListener('DOMContentLoaded', () => {
         el.addEventListener('mousedown', e => e.preventDefault());
     });
 });
+
+var filter = 'all';
+function sendRequest() {
+    fetch(`http://127.0.0.1:8080/hello-world/accounts?filter=${filter}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: 'John', email: 'john@example.com' })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
